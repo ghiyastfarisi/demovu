7b07252cedcad90cb2f1de51bc1b5293973f6416
ghiyastfarisi <ghiyast.farisi@gmail.com>
2019-05-25 21:33:53 +0700

    clean unused

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,16 @@
 <template>
   <section class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        basicnuxt
-      </h1>
       <h2 class="subtitle">
-        my basic nuxt with bulma framework
+        demovu
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
-  }
+  components: {}
 }
 </script>
 
@@ -44,16 +24,6 @@ export default {
   text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
 .subtitle {
   font-weight: 300;
   font-size: 42px;
@@ -62,7 +32,4 @@ export default {
   padding-bottom: 15px;
 }
 
-.links {
-  padding-top: 15px;
-}
 </style>
